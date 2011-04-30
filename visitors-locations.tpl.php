aa4ae73ef4c0867311942927a3ad8e18e10294a5
xlyz <xlyz@livenup.it>
2011-04-30 21:03:57 +0200

    initial import

---
+++ visitors-locations.tpl.php
@@ -0,0 +1,10 @@
+<?php
+// $Id: visitors-locations.tpl.php,v 1.1.2.6 2010/10/09 15:20:08 hass Exp $
+?>
+<div class="content">
+  <object width="100%" height="300" type="application/x-shockwave-flash" data="<?php print $piwik_url ?>/libs/open-flash-chart/open-flash-chart.swf?v2i" id="UserCountrygetCountryChart">
+    <param name="allowScriptAccess" value="always"/>
+    <param name="wmode" value="opaque"/>
+    <param name="flashvars" value="data-file=<?php print $data1_url ?>"/>
+  </object>
+</div>
---
+++ visitors-overview.tpl.php
@@ -0,0 +1,19 @@
+<?php
+?>
+<h2><?php
+  switch ($period) {
+    case 'week':
+      print t('Visitors in time period by week');
+      break;
+
+    default:
+      print t('Visitors in time period by day');
+  }
+?></h2>
+<div class="content">
+  <object width="100%" height="300" type="application/x-shockwave-flash" data="<?php print $piwik_url ?>/libs/open-flash-chart/open-flash-chart.swf?piwik=1.4" id="VisitsSummarygetLastVisitsGraphChart">
+    <param name="allowScriptAccess" value="always"/>
+    <param name="wmode" value="opaque"/>
+    <param name="flashvars" value="data-file=<?php print $data1_url ?>"/>
+  </object>
+</div>
