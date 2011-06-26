50b6dc1c2a1089aa9a74fadf655ebee6e69a01a5
xlyz <xlyz@livenup.it>
2011-06-26 21:50:28 +0200

    some more charts working again

--- piwik-reports.tpl.php
+++ piwik-reports.tpl.php
@@ -6,8 +6,8 @@
  * Available variables:
  * - $data_url: complete url with params to get selected report.
  */
-foreach($data_url as $url) {
+foreach($data_url as $data) {
 ?>
-</h2>
-<div class="widgetIframe"><iframe width="100%" height="350" <?php print 'src="' . $url; ?>" scrolling="no" frameborder="0" marginheight="0" marginwidth="0"></iframe></div>
+<h2><?php print $data['title']; ?></h2>
+<div class="widgetIframe"><iframe width="100%" height="350" <?php print 'src="' . $data['url']; ?>" scrolling="no" frameborder="0" marginheight="0" marginwidth="0"></iframe></div>
 <?php } ?>
