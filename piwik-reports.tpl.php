eabacc0d20f917ef9ae329167a74739966576141
guillaumev <guillaumev@849890.no-reply.drupal.org>
2018-08-24 14:17:41 -0700

    Issue #2085513 by guillaumev: Iframe should have scrolling enabled

--- piwik-reports.tpl.php
+++ piwik-reports.tpl.php
@@ -13,6 +13,6 @@
   else {
     ?>
     <h2><?php print $data['title']; ?></h2>
-    <div class="widgetIframe"><iframe width="100%" height="<?php print $data['iframe_height'] . '" src="' . $data['url']; ?>" scrolling="no" frameborder="0" marginheight="0" marginwidth="0"></iframe></div>
+    <div class="widgetIframe"><iframe width="100%" height="<?php print $data['iframe_height'] . '" src="' . $data['url']; ?>" scrolling="yes" frameborder="0" marginheight="0" marginwidth="0"></iframe></div>
     <?php }
   }
