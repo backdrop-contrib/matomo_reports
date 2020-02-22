a0070a737bbe0d4d8a12341aa21a232ed11b5f4a
Jason Flatt <drupal@oadaeh.net>
2020-02-22 11:22:44 -0800

    Removing forgotten debugging code.

--- matomo-reports.tpl.php
+++ matomo-reports.tpl.php
@@ -12,15 +12,12 @@ foreach($data_url as $data) {
     print $data['empty_text'];
   }
   else {
-    dpm('The contents of $data, one element at a time:');
     foreach ($data as $key => $value) {
-      dpm($value, $key);
     }
     ?>
     <h2><?php print $data['title']; ?></h2>
     <div class="widgetIframe">
       <iframe style="border: 0px none; width: 100%; height: <?php print $data['iframe_height'] . 'px;" src="' . $data['url'];?>" scrolling="auto" frameborder="0"></iframe>
-      <!--iframe src="http://matomo.techccessible.com/index.php?module=Widgetize&action=iframe&moduleToWidgetize=Dashboard&actionToWidgetize=index&idSite=22&period=week&date=yesterday" frameborder="0" marginheight="0" marginwidth="0" width="100%" height="100%"></iframe-->
     </div>
   <?php }
 }
